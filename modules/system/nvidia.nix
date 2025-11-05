cfcf2654a88330bd6ca85a4c5e251723007bedeb
PeaterPita <reedp1324@gmail.com>
2025-11-05 12:09:01 +0000

    Changed over to 580 Drivers

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -24,7 +24,7 @@ in
       # package = config.boot.kernelPackages.nvidiaPackages.beta; # Tried Stable, Beta, Production
       package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
         version = "580.95.05";
-        ha256_64bit = "sha256-hJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
+        sha256_64bit = "sha256-hJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
         sha256_aarch64 = "sha256-zLRCbpiik2fGDa+d80wqV3ZV1U1b4lRjzNQJsLLlICk=";
         openSha256 = "sha256-RFwDGQOi9jVngVONCOB5m/IYKZIeGEle7h0+0yGnBEI=";
         settingsSha256 = "sha256-F2wmUEaRrpR1Vz0TQSwVK4Fv13f3J9NJLtBe4UP2f14=";
