17d51985bbd152c68d73392501b9dae1574a2a9e
PeaterPita <reedp1324@gmail.com>
2025-11-10 12:43:16 +0000

    Cleaned up mongo module

--- modules/system/mongo.nix
+++ modules/system/mongo.nix
@@ -15,25 +15,8 @@ in
 
   config = lib.mkIf cfg.enable {
 
-    services.postgresql = {
-      enable = true;
-      ensureDatabases = [ "db_a1" ];
-      enableTCPIP = true;
-      ensureUsers = [
-        {
-          name = "db_a1";
-          ensureDBOwnership = true;
-        }
-      ];
-      authentication = pkgs.lib.mkOverride 10 ''
-        local all all trust
-        host all all 127.0.0.1/32 trust
-      '';
-    };
-
     environment.systemPackages = with pkgs; [
       mongodb-ce
-      pgweb
       mongodb-compass
     ];
   };
