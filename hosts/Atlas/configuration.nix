14cf53e8ac0b89b08f01c10e58b459a63bb191e4
PeaterPita <reedp1324@gmail.com>
2025-12-08 00:06:48 +0000

    Completely new Firefox module with more privacy focused policy changes

--- hosts/Atlas/configuration.nix
+++ hosts/Atlas/configuration.nix
@@ -13,6 +13,7 @@
 
   environment.systemPackages = with pkgs; [
     gimp
+    ghidra
   ];
   modules = {
     virt.enable = true;
--- modules/home/firefox/default.nix
+++ modules/home/firefox/default.nix
@@ -16,20 +16,100 @@ in
   config = lib.mkIf cfg.enable {
     programs.firefox = {
       enable = true;
+      profiles.default = {
+        settings = {
+          "browser.startup.homepage" = "about:home";
+
+          "browser.disableResetPrompt" = true;
+          "browser.download.panel.shown" = true;
+          "browser.feeds.showFirstRunUI" = false;
+          "browser.messaging-system.whatsNewPanel.enabled" = false;
+          "browser.rights.3.shown" = true;
+          "browser.shell.checkDefaultBrowser" = false;
+          "browser.shell.defaultBrowserCheckCount" = 1;
+          "browser.startup.homepage_override.mstone" = "ignore";
+          "browser.uitour.enabled" = false;
+          "startup.homepage_override_url" = "";
+          "trailhead.firstrun.didSeeAboutWelcome" = true;
+          "browser.bookmarks.restore_default_bookmarks" = false;
+          "browser.bookmarks.addedImportButton" = true;
+
+          # Features
+          "screenshots.browser.component.enabled" = false;
+          "browser.tabs.closeWindowWithLastTab" = false;
+
+          # Disable some telemetry
+          "app.shield.optoutstudies.enabled" = false;
+          "browser.discovery.enabled" = false;
+          "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+          "browser.newtabpage.activity-stream.telemetry" = false;
+          "browser.ping-centre.telemetry" = false;
+          "datareporting.healthreport.service.enabled" = false;
+          "datareporting.healthreport.uploadEnabled" = false;
+          "datareporting.policy.dataSubmissionEnabled" = false;
+          "datareporting.sessions.current.clean" = true;
+          "devtools.onboarding.telemetry.logged" = false;
+          "toolkit.telemetry.archive.enabled" = false;
+          "toolkit.telemetry.bhrPing.enabled" = false;
+          "toolkit.telemetry.enabled" = false;
+          "toolkit.telemetry.firstShutdownPing.enabled" = false;
+          "toolkit.telemetry.hybridContent.enabled" = false;
+          "toolkit.telemetry.newProfilePing.enabled" = false;
+          "toolkit.telemetry.prompted" = 2;
+          "toolkit.telemetry.rejected" = true;
+          "toolkit.telemetry.reportingpolicy.firstRun" = false;
+          "toolkit.telemetry.server" = "";
+          "toolkit.telemetry.shutdownPingSender.enabled" = false;
+          "toolkit.telemetry.unified" = false;
+          "toolkit.telemetry.unifiedIsOptIn" = false;
+          "toolkit.telemetry.updatePing.enabled" = false;
+
+          "privacy.trackingprotection.enabled" = true;
+          "dom.security.https_only_mode" = true;
+
+          "browser.newtabpage.activity-stream.feeds.topsites" = false;
+          "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+
+          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+
+          "browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts" = false;
+          "browser.newtabpage.blocked" = lib.genAttrs [
+            # Facebook
+            "4gPpjkxgZzXPVtuEoAL9Ig=="
+            # Wikipedia
+            "eV8/WsSLxHadrTL1gAxhug=="
+            # Reddit
+            "gLv0ja2RYVgxKdp0I5qwvA=="
+            # Amazon
+            "K00ILysCaEq8+bEqV/3nuw=="
+            # Twitter
+            "T9nJot5PurhJSy8n038xGA=="
+          ] (_: 1);
+
+        };
+
+        search = {
+          force = true;
+          engines = {
+
+            "NixOS Wiki" = {
+              urls = [ { template = "https://nixos.wiki/index.php?search={searchTerms}"; } ];
+              icon = "https://nixos.wiki/favicon.png";
+              updateInterval = 24 * 60 * 60 * 1000; # every day
+              definedAliases = [ "@nw" ];
+            };
+
+          };
+        };
+      };
 
       policies = {
         # Features
-        DontCheckDefaultBrowser = true;
         HardwareAcceleration = true;
-        GenerativeAI.enabled = false;
 
         # Privacy
         AutofillAddressEnabled = false;
         AutofillCreditCardEnabled = false;
-        DisableFirefoxStudies = true;
-        DisableFirefoxScreenshots = true;
-        DisableSetDesktopBackground = true;
-        DisableTelemetry = true;
 
         ExtensionSettings =
           let
@@ -43,42 +123,7 @@ in
               installation_mode = "force_installed";
               updates_disabled = true;
             };
-
           };
-
-        profiles.default = {
-          settings = {
-            "privacy.trackingprotection.enabled" = true;
-            "browser.startup.homepage" = "https://nixos.org";
-
-          };
-
-          search = {
-            force = true;
-            default = "DuckDuckGo";
-            engines = {
-              "Nix Packages" = {
-                urls = [
-                  {
-                    template = "https://search.nixos.org/packages";
-                    params = [
-                      {
-                        name = "channel";
-                        value = "25.05";
-                      }
-                      {
-                        name = "query";
-                        value = "{searchTerms}";
-                      }
-                    ];
-                    icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                    definedAliases = [ "@np" ];
-                  }
-                ];
-              };
-            };
-          };
-        };
       };
     };
   };
