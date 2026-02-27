8a87d73847884c3c08a954b44362d0f1f4adffa1
PeaterPita <90217572+PeaterPita@users.noreply.github.com>
2026-02-27 08:07:15 +0000

    Secrets ++ Added Hermes keys to sops ++ New services.yaml secrets file ++ sops for proxmox and gitea tokens ++ New homepage widgets

--- flake.nix
+++ flake.nix
@@ -31,6 +31,7 @@
         packages = with pkgs; [
           sops
           age
+          openssl
         ];
       };
     };
--- modules/system/servers/homepage.nix
+++ modules/system/servers/homepage.nix
@@ -15,7 +15,7 @@ in
     enable = lib.mkEnableOption "Enable the Homepage dashboard ";
 
     groups = lib.mkOption {
-      type = lib.types.attrsOf (lib.types.attrsOf lib.types.attrs);
+      type = lib.types.attrsOf (lib.types.listOf lib.types.attrs);
       default = { };
     };
   };
@@ -23,22 +23,35 @@ in
   config = lib.mkIf cfg.enable {
     networking.firewall.allowedTCPPorts = [ 8082 ];
 
+    sops.secrets."proxmox-api-token" = {
+      sopsFile = ../../../secrets/services.yaml;
+    };
+
+    sops.secrets."gitea-token" = {
+      sopsFile = ../../../secrets/services.yaml;
+    };
+
+    sops.templates."homepage.env".content = ''
+      HOMEPAGE_VAR_PROXMOX_TOKEN=${config.sops.placeholder."proxmox-api-token"}
+      HOMEPAGE_VAR_GITEA_TOKEN=${config.sops.placeholder."gitea-token"}
+    '';
+
     services.homepage-dashboard = {
       enable = true;
-      allowedHosts = "home.arpa,homepage.${vars.baseDomain},${vars.baseDomain}:8082,127.0.0.1:8082";
+      environmentFile = config.sops.templates."homepage.env".path;
+
+      allowedHosts = lib.concatStringsSep "," [
+        vars.baseDomain
+        "${vars.baseDomain}:8082"
+        "127.0.0.1:8082"
+        vars.ingressIP
+      ];
 
       settings = {
         title = "PeaterPita Home";
         theme = "dark";
       };
       widgets = [
-        {
-          resources = {
-            cpu = true;
-            memory = true;
-            disk = "/";
-          };
-        }
 
         {
           search = {
@@ -47,33 +60,61 @@ in
           };
 
         }
+
       ];
 
-      services = lib.mkMerge [
-        (lib.mapAttrsToList (name: items: { "${name}" = items; }) cfg.groups)
-        [
-
-          # {
-          #   "Games" = [
-          #     {
-          #       "Pterodactyl Panel" = {
-          #         icon = "pterodactyl.png";
-          #         href = "https://panel.home.arpa";
-          #         description = "Minecraft server hosting";
-          #         widget = {
-          #           type = "pterodactyl";
-          #           url = "http://192.168.1.Y";
-          #           key = "";
-          #         };
-          #       };
-          #     }
-          #   ];
-          # }
-
-        ]
-      ];
+      services = lib.mapAttrsToList (name: items: { "${name}" = items; }) cfg.groups;
 
     };
+    homelab.services.homepage.groups = {
+      "Security" = [
+        {
+          traefik = {
+            icon = "traefik.png";
+            href = "https://traefik.${vars.baseDomain}";
+            description = "Reverse Proxy";
+            ping = "https://${vars.ingressIP}:";
+          };
+        }
+      ];
 
+      "Games" = [
+
+        {
+          "Pterodactyl Panel" = {
+            icon = "pterodactyl.png";
+            href = "https://panel.home.arpa";
+            description = "Minecraft server hosting";
+            widget = {
+              type = "pterodactyl";
+              url = "http://192.168.1.Y";
+              key = "";
+            };
+          };
+        }
+
+        {
+          "Hypixel" = {
+            icon = "minecraft.png";
+            description = "The Hypixel minecraft server";
+            widget = {
+              type = "minecraft";
+              url = "udp://play.hypixel.net";
+            };
+          };
+        }
+
+        {
+          "The Universe Network" = {
+            icon = "minecraft.png";
+            description = "The UniverseNetwork minecraft server";
+            widget = {
+              type = "minecraft";
+              url = "udp://play.theuniverse.network";
+            };
+          };
+        }
+      ];
+    };
   };
 }
