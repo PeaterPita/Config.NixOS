12ca00d026d2102344305802f99f16044fd6f8b0
PeaterPita <90217572+PeaterPita@users.noreply.github.com>
2026-04-23 22:07:54 +0100

    Mealie OIDC ++ Mealie now has OIDC

--- modules/system/servers/mealie.nix
+++ modules/system/servers/mealie.nix
@@ -1,6 +1,5 @@
 {
   config,
-  inputs,
   lib,
   ...
 }:
@@ -14,13 +13,8 @@ in
   options.homelab.services.mealie = {
     enable = lib.mkEnableOption "Enable the Mealie recipe storage and sharing platform";
     port = lib.mkOption { default = 9004; };
+    domain = lib.mkOption { default = "meals.${vars.baseDomain}"; };
   };
-
-  imports = [
-
-    inputs.disko.nixosModules.disko
-
-  ];
 
   config = lib.mkIf cfg.enable {
 
@@ -28,33 +22,53 @@ in
       {
         Mealie = {
           icon = "mealie.png";
-          href = "http://mealie.${vars.baseDomain}";
+          href = "http://${cfg.domain}";
+          description = "Recipes & Meals";
           ping = "http://127.0.0.1:${builtins.toString cfg.port}";
         };
       }
     ];
 
-    # disko.devices.zpool.tank.datasets."mealie" = {
-    #   type = "zfs_fs";
-    #   mountpoint = "/mnt/mealie";
-    # };
+    homelab.services.authelia.oidc = [
+      {
+        client_id = "mealie";
+        client_name = "mealie";
+        client_secret = "$pbkdf2-sha512$310000$BdOjxueVb7q4XcGk9FzbIQ$zvunGtuGX9DRJAd5sPbOQzn3x/b4lVrKro3IkLU9u1kxsecS7lx0gARSd7Qdlif2NDhqD77ioCpNSNkgAMAR0w";
+        public = false;
+        authorization_policy = "one_factor";
+        grant_types = [ "authorization_code" ];
+        redirect_uris = [
+          "https://${cfg.domain}/login"
+          "http://${cfg.domain}/login"
+        ];
+        scopes = [
+          "openid"
+          "profile"
+          "email"
+          "groups"
+        ];
+        token_endpoint_auth_method = "client_secret_basic";
+      }
+    ];
 
-    # fileSystems."/var/lib/mealie" = {
-    #   device = "/mnt/mealie";
-    #   options = [ "bind" ];
-    #   depends = [ "/mnt/mealie" ];
-    # };
-
-    services.postgresql = {
-      enable = true;
-      ensureDatabases = [ "mealie" ];
-      ensureUsers = [
-        {
-          name = "mealie";
-          ensureDBOwnership = true;
-        }
-      ];
+    sops.secrets."mealie/oidc_secret" = {
+      sopsFile = ../../../secrets/services.yaml;
     };
+
+    sops.templates."mealie.env".content = ''
+      OIDC_CLIENT_SECRET=${config.sops.placeholder."mealie/oidc_secret"}
+    '';
+
+    # services.postgresql = {
+    #   enable = true;
+    #   ensureDatabases = [ "mealie" ];
+    #   ensureUsers = [
+    #     {
+    #       name = "mealie";
+    #       ensureDBOwnership = true;
+    #     }
+    #   ];
+    # };
 
     networking.firewall.allowedTCPPorts = [ cfg.port ];
 
@@ -62,13 +76,30 @@ in
       enable = true;
       port = cfg.port;
       settings = {
-        DB_ENGINE = "postgres";
-        POSTGRES_SERVER = "127.0.0.1";
-        POSTGRES_USER = "mealie";
-        POSTGRES_DB = "mealie";
+        # DB_ENGINE = "postgres";
+        # POSTGRES_SERVER = "127.0.0.1";
+        # POSTGRES_USER = "mealie";
+        # POSTGRES_DB = "mealie";
 
         TZ = "GMT";
+        BASE_URL = "https://${cfg.domain}";
+
+        OIDC_AUTH_ENABLED = "true";
+        OIDC_CONFIGURATION_URL = "https://auth.${vars.baseDomain}/.well-known/openid-configuration";
+
+        OIDC_CLIENT_ID = "mealie";
+
+        OIDC_AUTO_REDIRECT = "false";
+
+        OIDC_GROUPS_CLIAM = "groups";
+        OIDC_ADMIN_GROUP = "admin";
+        OIDC_USER_GROUP = "family";
+
+        OIDC_PROVIDER_NAME = "Authelia";
+        OIDC_REMEMBER_ME = "true";
+        OIDC_SIGNING_ALGORITHM = "RS256";
       };
+      credentialsFile = config.sops.templates."mealie.env".path;
     };
   };
 
