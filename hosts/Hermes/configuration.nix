25060ebca2aca1025e186fd50b060cb7148dddba
PeaterPita <90217572+PeaterPita@users.noreply.github.com>
2026-05-20 14:24:53 +0100

    Added back dns stub

--- hosts/Hermes/configuration.nix
+++ hosts/Hermes/configuration.nix
@@ -76,10 +76,10 @@ in
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ4UunpG+zwcD8K6yKG0Tl9DOG4fbl+tb0MjIVIOGNyp"
     ];
   };
-  # services.resolved.extraConfig = ''
-  #   DNS=1.1.1.1
-  #   DNSStubListener=no
-  # '';
+  services.resolved.extraConfig = ''
+    DNS=1.1.1.1
+    DNSStubListener=no
+  '';
 
   homelab.services = {
     glances.enable = true;
