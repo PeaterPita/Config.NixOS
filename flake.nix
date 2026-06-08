835cc4bc415dd5b4945ec641c373fd7e56b94f4d
PeaterPita <90217572+PeaterPita@users.noreply.github.com>
2026-06-08 03:34:10 +0100

    Removal of Host.nix files ++ Removed the experiment of automagically identifiying hosts ++ Removed per host `host.nix` files ++ Simplified flake.nix

--- flake.nix
+++ flake.nix
@@ -7,33 +7,21 @@
     ####################################################
     { self, nixpkgs, ... }@inputs:
     let
-      lib = nixpkgs.lib;
-
       mkSystem = import ./utils/mkSystem.nix {
         inherit
           inputs
           self
           ;
       };
-
-      hostNames = builtins.filter (name: name != "common") (
-        builtins.attrNames (lib.filterAttrs (_: type: type == "directory") (builtins.readDir ./hosts))
-      );
-
-      nixosConfigurations = builtins.listToAttrs (
-        map (hostname: {
-          name = hostname;
-          value =
-            let
-              meta = import ./hosts/${hostname}/host.nix;
-            in
-            mkSystem hostname meta.users meta.system;
-        }) hostNames
-      );
-
     in
     {
-      inherit nixosConfigurations;
+      nixosConfigurations = {
+        Atlas = mkSystem "Atlas" [ "peaterpita" ] "x86_64-linux";
+        Icarus = mkSystem "Icarus" [ "peaterpita" ] "x86_64-linux";
+
+        Olympus = mkSystem "Olympus" [ "peaterpita" ] "x86_64-linux";
+        Hermes = mkSystem "Hermes" [ ] "x86_64-linux";
+      };
 
       checks."x86_64-linux" = builtins.mapAttrs (
         _: cfg: cfg.config.system.build.toplevel
