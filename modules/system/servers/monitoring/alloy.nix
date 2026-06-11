7b276c14e3ce556f6b6689c2f7fa0b294d0e184a
PeaterPita <90217572+PeaterPita@users.noreply.github.com>
2026-06-11 16:06:11 +0100

    Alloy relabel rule

--- modules/system/servers/monitoring/alloy.nix
+++ modules/system/servers/monitoring/alloy.nix
@@ -19,12 +19,23 @@ in
   config = lib.mkIf cfg.enable {
     services.alloy = {
       enable = true;
+      extraFlags = [ "--disable-reporting" ];
 
     };
 
     environment.etc."alloy/config.alloy".text = ''
+
+        loki.relabel "journal" {
+            forward_to = []
+            rule {
+                source_labels = ["__journal__systemd_unit"]
+                target_label = "unit"
+            }
+        }
+
         loki.source.journal "journal" {
             forward_to = [loki.write.default.receiver]
+            relabel_rules = loki.relabel.journal.rules
             labels = {
                 host = "${config.networking.hostName}",
                 job = "journal",
