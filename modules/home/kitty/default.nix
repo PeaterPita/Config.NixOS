f7220cf200cc1891f66282dd902611e2178ced5f
PeaterPita <90217572+PeaterPita@users.noreply.github.com>
2026-06-14 23:18:39 +0100

    Kitty Tweaks ++ Improved Kitty experience in hopes of replacing zelliji ++ New keybinds for pane and tab managment ++ QOL tweaks like copy on select ++ Starship module enabled

--- modules/home/kitty/default.nix
+++ modules/home/kitty/default.nix
@@ -14,6 +14,8 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+    modules.starship.enable = true;
+
     programs.kitty = {
       enable = true;
       font = {
@@ -23,10 +25,46 @@ in
       };
       shellIntegration.enableZshIntegration = true;
       themeFile = "VibrantInk";
+      keybindings = {
+        "ctrl+c" = "copy_or_interrupt";
+
+        "ctrl+shift+n" = "launch --location=split --cwd=current";
+
+        "ctrl+shift+t" = "launch --type=tab";
+
+        "ctrl_shift+f" = "toggle_layout stack";
+        "ctrl_shift+r" = "start_resizing_window";
+
+        "ctrl+shift+left" = "neighboring_window left";
+        "ctrl+shift+right" = "neighboring_window right";
+        "ctrl+shift+up" = "neighboring_window up";
+        "ctrl+shift+down" = "neighboring_window down";
+
+      }
+      // builtins.listToAttrs (
+        map (num: {
+          name = "ctrl+shift+${toString num}";
+          value = "goto_tab ${toString num}";
+        }) (lib.range 1 9)
+      );
       settings = {
+
+        remember_window_size = "yes";
+        inactive_text_alpha = "-0.4";
+
+        tab_bar_style = "powerline";
+        tab_powerline_style = "slanted";
+        tab_title_template = "{index}: {title}";
+        active_tab_title_template = ">> {index} <<";
+
+        copy_on_select = "clipboard";
+        strip_trailing_spaces = "smart";
+
+        notify_on_cmd_finish = "unfocused";
+
         confirm_os_window_close = "0";
-        map = "ctrl+c copy_or_interrupt";
         background_opacity = "0.8";
+        enabled_layouts = "splits,stack";
       };
     };
   };
