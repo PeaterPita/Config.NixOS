b07fa44e20769d2819fef973b25bd5fcb1316ab1
PeaterPita <90217572+PeaterPita@users.noreply.github.com>
2026-06-28 10:34:27 +0100

    Greetd fixes ++ removed running as personal user to allow cache writing ++ remember session flag addedd

--- modules/system/DE/wayland.nix
+++ modules/system/DE/wayland.nix
@@ -30,8 +30,7 @@ in
       enable = true;
       settings = {
         default_session = {
-          command = "${pkgs.tuigreet}/bin/tuigreet --time --remember";
-          user = "peaterpita";
+          command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session";
         };
       };
     };
