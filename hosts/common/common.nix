a522cd42969b3948a24f11c5afdc75803e1ab81c
PeaterPita <90217572+PeaterPita@users.noreply.github.com>
2026-07-07 13:57:36 +0100

    Common changes ++ Servers no longer on latest kernel ++ Servers no longer have sysrq ++ Servers no longer have memtest ++ Servers no longer have desktop boot flare

--- hosts/common/common.nix
+++ hosts/common/common.nix
@@ -1,85 +1,96 @@
 {
+  config,
   pkgs,
+  lib,
   ...
 }:
 
 {
   imports = [ ./common-programs.nix ];
-  boot = {
 
-    kernel.sysctl."kernel.sysrq" = 1;
-    consoleLogLevel = 3;
-    initrd.verbose = false;
-    initrd.systemd.enable = true;
+  config = lib.mkMerge [
 
-    kernelPackages = pkgs.linuxPackages_latest;
-
-    kernelParams = [
-      "quiet"
-      "splash"
-      "boot.shell_on_fail"
-    ];
-
-    loader = {
-
-      systemd-boot.enable = true;
-      systemd-boot.memtest86.enable = true;
-      efi.canTouchEfiVariables = true;
-      timeout = 0; # <- Make it so that the generation choice doesnt appear UNLESS key is held during boot sequence.
-    };
-  };
-
-  services = {
-
-    journald.extraConfig = "SystemMaxUse=100M";
-
-    resolved.enable = true;
-    openssh = {
-      enable = true;
-      settings = {
-        PasswordAuthentication = true;
-        KbdInteractiveAuthentication = true;
+    (lib.mkIf config.system.isDesktop {
+      boot = {
+        kernel.sysctl."kernel.sysrq" = 1;
+        kernelPackages = pkgs.linuxPackages_latest;
+        kernelParams = [
+          "quiet"
+          "splash"
+        ];
+        loader.systemd-boot.memtest86.enable = true;
       };
-    };
+    })
+    {
 
-  };
+      boot = {
+        consoleLogLevel = 3;
+        initrd.verbose = false;
+        initrd.systemd.enable = true;
 
-  time.timeZone = "Europe/London";
-  i18n.defaultLocale = "en_GB.UTF-8";
+        kernelParams = [
+          "boot.shell_on_fail"
+        ];
 
-  nixpkgs.config = {
-    allowUnfree = true;
-  };
+        loader = {
+          systemd-boot.enable = true;
+          efi.canTouchEfiVariables = true;
+          timeout = 0; # <- Make it so that the generation choice doesnt appear UNLESS key is held during boot sequence.
+        };
+      };
 
-  nix = {
-    settings = {
-      substituters = [
-        "https://cache.peaterpita.com"
-        "https://noctalia.cachix.org"
-      ];
-      trusted-public-keys = [
-        "cache.peaterpita.com-1:HFufcQT6KtSMSJKFu9UCDQ2cSD6k0LmjvAMxm4KFciU="
-        "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
-      ];
+      services = {
+        journald.extraConfig = "SystemMaxUse=100M";
 
-      trusted-users = [ "peaterpita" ];
-      auto-optimise-store = true;
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-    };
+        resolved.enable = true;
+        openssh = {
+          enable = true;
+          settings = {
+            PasswordAuthentication = true;
+            KbdInteractiveAuthentication = true;
+          };
+        };
 
-    registry.temps.to = {
-      type = "git";
-      url = "git+ssh://git@github.com/PeaterPita/nixtemplates.git";
-    };
+      };
 
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
-    };
-  };
+      time.timeZone = "Europe/London";
+      i18n.defaultLocale = "en_GB.UTF-8";
+
+      nixpkgs.config = {
+        allowUnfree = true;
+      };
+
+      nix = {
+        settings = {
+          substituters = [
+            "https://cache.peaterpita.com"
+            "https://noctalia.cachix.org"
+          ];
+          trusted-public-keys = [
+            "cache.peaterpita.com-1:HFufcQT6KtSMSJKFu9UCDQ2cSD6k0LmjvAMxm4KFciU="
+            "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+          ];
+
+          trusted-users = [ "peaterpita" ];
+          auto-optimise-store = true;
+          experimental-features = [
+            "nix-command"
+            "flakes"
+          ];
+        };
+
+        registry.temps.to = {
+          type = "git";
+          url = "git+ssh://git@github.com/PeaterPita/nixtemplates.git";
+        };
+
+        gc = {
+          automatic = true;
+          dates = "weekly";
+          options = "--delete-older-than 7d";
+        };
+      };
+    }
+  ];
 
 }
