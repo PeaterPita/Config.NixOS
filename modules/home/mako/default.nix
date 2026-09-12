69f946f23caa030fe05482f6a165a61a0d33e9be
PeaterPita <90217572+PeaterPita@users.noreply.github.com>
2026-09-12 11:06:05 +0100

    Mako Matugen Template

--- modules/home/mako/default.nix
+++ modules/home/mako/default.nix
@@ -14,12 +14,41 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    modules.starship.enable = true;
     home.packages = [ pkgs.monocraft ];
+
+    modules.matugen.templates."mako" = {
+      outputPath = "~/.config/mako/matugen.colors";
+      reloadCmd = "makoctl reload";
+
+      text = ''
+        background-color={{colors.on_primary.default.hex}}
+        text-color={{colors.tertiary.default.hex}}
+        border-color={{colors.tertiary_container.default.hex}}
+
+        [mode=do-not-disturb]
+        invisible=1
+        on-notify=none
+
+        [mode=silent]
+        on-notify=none
+
+        [urgency=high]
+        border-color={{colors.error_container.default.hex}}
+      '';
+    };
 
     services.mako = {
       enable = true;
       settings = {
+
+        actions = true;
+        anchor = "top-right";
+        default-timeout = 5000;
+        font = "monocraft 10";
+        icons = true;
+        layer = "overlay";
+
+        include = "~/.config/mako/matugen.colors";
       };
     };
   };
